a1c83c91cbb7ce6b474db96dc4340b88aeb56e85
dacinve <78516378+dacinve@users.noreply.github.com>
2022-04-24 00:01:00 -0400

    Add page

---
+++ app1.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    const navToggle = document.querySelector('.nav_toggle');
+    const nav = document.querySelector('.nav');
+    const navLinks = document.querySelector('.nav-link');
+
+    navToggle.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+    }); 
+    navToggle();
+    })
